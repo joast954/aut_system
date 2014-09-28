02b9a27fd76b54f061dc8be6d6ac89f0aeac3639
joast954 <strom.joakim@gmail.com>
2014-09-28 14:35:53 +0200

    Initial clean Rails 4 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuthSystem::Application.config.secret_key_base = 'b5d7ec697d0b3475662237d7b0a56562ebb136231188c351fb739dbc11671b5b7bc10f4310dedccad19e687da4af7f400d1e3efede533864456ca4a85429a265'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AuthSystem::Application.config.session_store :cookie_store, key: '_auth_system_session'
